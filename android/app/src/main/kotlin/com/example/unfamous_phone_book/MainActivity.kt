acd51cf40177c2a81bda22ce451afcb544d66959
SeppoVolsk <104652126+SeppoVolsk@users.noreply.github.com>
2022-08-05 20:24:25 +0300

    - initial commit - Pixel4 AVD with PlayStore created - google.ApiException10 - not solved

---
+++ android/app/src/main/kotlin/com/example/unfamous_phone_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.unfamous_phone_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/enter_screen/enter_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EnterScreen extends StatelessWidget {
+  const EnterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
