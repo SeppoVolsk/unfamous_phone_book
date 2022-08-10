f91cc5eeabaeee2e0d64fd4cc7976e57753b63af
SeppoVolsk <104652126+SeppoVolsk@users.noreply.github.com>
2022-08-10 21:00:24 +0300

    added some methofs to EnterScreenRepository

--- lib/screens/enter_screen/enter_screen_bloc/enter_screen_repository.dart
+++ lib/screens/enter_screen/enter_screen_bloc/enter_screen_repository.dart
@@ -1,1 +1,31 @@
-class IEnterScreenRepository {}
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:unfamous_phone_book/screens/enter_screen/enter_screen_bloc/enterscreenentity.dart';
+
+class IEnterScreenRepository {
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    // Optional clientId
+    // clientId: '40564...........apps.googleusercontent.com',
+    scopes: <String>[
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+
+  GoogleSignInAccount? currentUser;
+
+  EnterScreenEntity logIn() {
+    _googleSignIn.signInSilently();
+    return EnterScreenEntity(user: currentUser);
+  }
+
+  Future<EnterScreenEntity> logOut() async {
+    await _googleSignIn.disconnect();
+    return EnterScreenEntity(user: null);
+  }
+
+  void checkCurrentUser() {
+    _googleSignIn.onCurrentUserChanged.listen((account) {
+      currentUser = account;
+    });
+  }
+}
