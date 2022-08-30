fa92686cc319731596ce0e1785bfe14ac46f16d1
SeppoVolsk <104652126+SeppoVolsk@users.noreply.github.com>
2022-08-30 20:48:26 +0300

    detail sheet widget in progress

---
+++ lib/screens/detail_sheet/detail_sheet_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:unfamous_phone_book/ui_components.dart';
+
+class DetailSheetWidget extends StatefulWidget {
+  DetailSheetWidget({Key? key}) : super(key: key);
+
+  @override
+  State<DetailSheetWidget> createState() => _DetailSheetWidgetState();
+}
+
+class _DetailSheetWidgetState extends State<DetailSheetWidget> {
+  final cornerRadius = const Radius.circular(16);
+  final widgetColor = UiAssets.randomColor();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+          decoration: BoxDecoration(
+              color: widgetColor,
+              borderRadius: BorderRadius.only(
+                  topLeft: cornerRadius, topRight: cornerRadius)),
+          height: 250,
+          //color: widgetColor,
+          child: Center(
+              child: Column(children: [
+            const Spacer(),
+            const DetailSheetInputField(),
+            const Spacer(),
+            const DetailSheetInputField(),
+            const Spacer(),
+            const DetailSheetInputField(),
+            const Spacer(),
+            IconButton(
+              icon: const Icon(Icons.done_outline_sharp),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ]))),
+    );
+  }
+}
+
+class DetailSheetInputField extends StatelessWidget {
+  const DetailSheetInputField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        decoration: InputDecoration(
+      contentPadding: const EdgeInsets.all(10),
+      fillColor: Colors.white,
+      filled: true,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: const BorderSide(
+          width: 0,
+          style: BorderStyle.none,
+        ),
+      ),
+    ));
+  }
+}
